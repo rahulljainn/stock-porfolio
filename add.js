cc8cfca859b26f6fcfe6a464e9554acbf33634f1
Rahul Jain <rahulllljainnnn@gmail.com>
2022-09-21 03:39:38 +0530

    update errorhandling-negativevalue&blank

--- add.js
+++ add.js
@@ -4,14 +4,23 @@ var currentPrice=document.querySelector("#current-price");
 var checkBtn=document.querySelector("#submitbtn");
 var output=document.querySelector("#outputbox");
 
+
+
 checkBtn.addEventListener('click',clickhandler)
 
 function clickhandler(){
-    var ip=intialPrice.value;
-    var qty=quantitystck.value;
-    var curr=currentPrice.value;
-
-    calculateprofitandloss(ip,qty,curr);
+    if(intialPrice.value >0 && quantitystck.value >0 && currentPrice.value >0 )
+    {var ip=intialPrice.value;
+        var qty=quantitystck.value;
+        var curr=currentPrice.value;
+    
+        calculateprofitandloss(ip,qty,curr);}
+        else if( intialPrice.value<0 || quantitystck.value<0 || currentPrice.value<0){ 
+            alert("Negative value are not allowed")
+        }else{
+            alert("Please Enter valid information")
+        }
+    
 }
 
 function calculateprofitandloss(intial,quantity,current){
@@ -19,12 +28,12 @@ function calculateprofitandloss(intial,quantity,current){
         var loss=(intial-current)*quantity;
         var losspercentage=(loss/intial)*100;
 
-        output.innerText=`Your loss is ${loss} and your Loss-percentage is ${losspercentage}% `
+        output.innerText=`Your loss is ${loss} and your Loss-percentage is ${losspercentage.toFixed(2)}% `
         }else if(current>intial){
         var profit=(current-intial)*quantity;
         var profitpercentage=(profit/intial)*100;
 
-        output.innerText=`Your profit is ${profit} and your Profit-percentage is ${profitpercentage}%`
+        output.innerText=`Your profit is ${profit} and your Profit-percentage is ${profitpercentage.toFixed(2)}%`
 
 }else{
     output.innerText="There is no loss no gain"
